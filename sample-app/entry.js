8cee3800f487a78d15daef576773afbf829d5fe6
roopkt <roopkt@gmail.com>
2017-03-01 18:41:13 -0500

    using webpack-dev-server

--- sample-app/entry.js
+++ sample-app/entry.js
@@ -1,3 +1,4 @@
 ﻿require("./style.css");
 document.write(require("./content.js"));
 document.write('<br>watch is on ye !');
+document.write('<br> using webpack-dev-server! just watch your web app it is refreshing live after you save your chagnes.');
